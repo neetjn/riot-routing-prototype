5c47d1b190c29ebcca2362a92429d9aba2ca1d9e
John Nolette <jnolette@transparent.com>
2019-11-10 00:14:18 -0500

    Added header nav example

--- src/app.js
+++ src/app.js
@@ -7,4 +7,11 @@ const component = riot.component(Page)
 import reload from '@riotjs/hot-reload'
 reload(component)
 
+// NOT PART OF ACTUAL PACKAGE
+// GLOBAL STATE EXAMPLE
+const router = { route: null, components: [] }
+riot.install((component) => {
+  component.router = router
+})
+
 component(document.querySelector('#app'))
